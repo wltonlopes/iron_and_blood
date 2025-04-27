f05e0ac59f2c5c2281036b2458ea58bf4ce396d3
wltonlopes <wltonlopesmetal@gmail.com>
2025-04-27 05:26:08 -0300

    atualizados

--- gui/pregame/backgrounds/pyrogenesis-ww1-4.js
+++ gui/pregame/backgrounds/pyrogenesis-ww1-4.js
@@ -1,8 +1,13 @@
 g_BackgroundLayerData.push(
     [
         {
-            "offset": (time, width) => 0,
+            "offset": () => 0,
             "sprite": "pyrogenesis-ww1-4",
             "tiling": true,
         },
-    ]);
+		{
+			"offset": (time, width) => 0.05 * width * Math.cos(0.1 * time),
+			"sprite": "background-viking-02",
+			"tiling": true
+		}
+	]);
